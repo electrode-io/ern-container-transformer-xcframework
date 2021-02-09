80dbd1874a132f50527b337b099090385773afa7
Benoit Lemaire <lemaireb@gmail.com>
2021-02-09 10:05:29 -0800

    Cleanup and fix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-export * from './XCFrameworkTransformer'
+import path from 'path'
+import shell from 'shelljs'
+export default class XCFrameworkTransformer {
+  /**
+   * Name of this transformer
+   */
+  get name(): string {
+    return 'xcframework'
+  }
+
+  /**
+   * Supported platforms
+   */
+  get platforms(): string[] {
+    return ['ios']
+  }
+
+  /**
+   * Transform the container
+   */
+  public async transform({
+    containerPath
+  }: {
+    containerPath: string
+  }) {
+    const scriptPath = path.join(__dirname, "generate-xcframework.sh");
+    const scriptResult = shell.exec(scriptPath, {cwd:containerPath})
+    if (scriptResult.code !== 0) {
+      this.throwError(`script execution failed with code ${scriptResult.code}`)
+    }
+  }
+
+  public throwError(msg: string) {
+    throw new Error(`[XCFrameworkTransformer] ${msg}`)
+  }
+}
