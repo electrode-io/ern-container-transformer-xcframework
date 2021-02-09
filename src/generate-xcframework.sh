816788875ba041efbeff6c077d6a5e477f51c1a9
Benoit Lemaire <lemaireb@gmail.com>
2021-02-09 10:42:51 -0800

    Fix xcframework generation script

--- src/generate-xcframework.sh
+++ src/generate-xcframework.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 xcodebuild archive \
 -workspace ElectrodeContainer.xcworkspace \
 -scheme ElectrodeContainer \
@@ -6,7 +5,7 @@ xcodebuild archive \
 -archivePath build/ElectrodeContainer-Device \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-​
+
 xcodebuild archive \
 -workspace ElectrodeContainer.xcworkspace \
 -scheme ElectrodeContainer \
@@ -14,9 +13,9 @@ xcodebuild archive \
 -archivePath build/ElectrodeContainer-Simulator \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-​
+
 xcodebuild -create-xcframework \
 -framework build/ElectrodeContainer-Device.xcarchive/Products/Library/Frameworks/ElectrodeContainer.framework \
 -framework build/ElectrodeContainer-Simulator.xcarchive/Products/Library/Frameworks/ElectrodeContainer.framework \
 -output ElectrodeContainer.xcframework
-​
+
